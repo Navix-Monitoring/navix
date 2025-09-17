fb4c26f779370767ad3a713c7d2aa656cbedcc39
gustavokeniti <gustavo.keniti@sptech.school>
2025-09-17 18:48:07 -0300

    fix: script bd

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -2,88 +2,190 @@ DROP DATABASE IF EXISTS navix;
 CREATE DATABASE navix;
 USE navix;
 
-CREATE TABLE empresa (
-	id_empresa INT NOT NULL auto_increment,
-    razaoSocial VARCHAR(50) NOT NULL,
-    cnpj VARCHAR(14) NOT NULL,
-    emailCorporativo VARCHAR(50) not null,
-    senha VARCHAR(250) not null,
-	caminhoImagem VARCHAR(500) NOT NULL DEFAULT '../assets/img/foto-usuario.png',
-    PRIMARY KEY (id_empresa),
-    UNIQUE (emailCorporativo, cnpj)
+CREATE TABLE endereco (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    rua VARCHAR(300) NOT NULL,
+    numero INT NOT NULL,
+    cep CHAR(8) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(200) NOT NULL,
+    estado VARCHAR(100) NOT NULL,
+    pais VARCHAR(200) NOT NULL
 );
 
-create table funcionario (
-	id_funcionario INT NOT NULL auto_increment,
+CREATE TABLE empresa (
+	id_empresa INT NOT NULL AUTO_INCREMENT,
+    razaoSocial VARCHAR(50) NOT NULL,
+    cnpj VARCHAR(14) NOT NULL,
+    fkEndereco INT NOT NULL,
+    PRIMARY KEY (id_empresa),
+    CONSTRAINT fkendereco FOREIGN KEY (fkEndereco) REFERENCES endereco(id)
+);
+
+CREATE TABLE funcionario (
+	id_funcionario INT NOT NULL AUTO_INCREMENT,
     fkEmpresa INT,
     nome VARCHAR(50) NOT NULL,
     sobrenome VARCHAR(50) NOT NULL,
     telefone VARCHAR(11),
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(250) NOT NULL,
-    cargo VARCHAR(30) NOT NULL,
+    cargo VARHCAR(100) NOT NULL,
 	caminhoImagem VARCHAR(500) NOT NULL DEFAULT '../assets/img/foto-usuario.png',
     CONSTRAINT chk_telefone CHECK (CHAR_LENGTH(telefone) IN (10, 11)),
     PRIMARY KEY (id_funcionario),
     UNIQUE (email), 
-    CONSTRAINT fk_empresa_funcionario FOREIGN KEY (fkEmpresa) REFERENCES empresa(id_empresa), 
-	KEY ix_fkEmprsa (fkEmpresa) 
+    CONSTRAINT fk_empresa_funcionario FOREIGN KEY (fkEmpresa) REFERENCES empresa(id_empresa)
 );
 
-create table lote(
-	id_lote int not null auto_increment,
-    codigo_lote varchar(50) not null, 
-    data_fabricacao date,
-    primary key(id_lote),
-    unique(codigo_lote)
+CREATE TABLE lote (
+	id_lote INT NOT NULL AUTO_INCREMENT,
+    codigo_lote VARCHAR(50) NOT NULL, 
+    data_fabricacao DATE,
+    PRIMARY KEY(id_lote),
+    UNIQUE(codigo_lote)
 );
 
-create table veiculo (
-	id_veiculo int not null auto_increment,
-    fkempresa int not null,
-    fklote int not null,
-    modelo varchar(50),
-    pilotoAuto_versao varchar(20),
-    data_ativacao date,
-    status enum('ativo', 'manutenção', 'inativo'),
-    primary key (id_veiculo),
-    constraint fk_veiculo_empresa foreign key (fkempresa) references empresa(id_empresa),
-    constraint fk_veiculo_lote foreign key (fklote) references lote(id_lote)
+CREATE TABLE veiculo (
+	id_veiculo INT NOT NULL AUTO_INCREMENT,
+    fkempresa INT NOT NULL,
+    fklote INT NOT NULL,
+    modelo VARCHAR(50),
+    pilotoAuto_versao VARCHAR(20),
+    data_ativacao DATE,
+    status ENUM('ativo', 'manutenção', 'inativo'),
+    PRIMARY KEY (id_veiculo),
+    CONSTRAINT fk_veiculo_empresa FOREIGN KEY (fkempresa) REFERENCES empresa(id_empresa),
+    CONSTRAINT fk_veiculo_lote FOREIGN KEY (fklote) REFERENCES lote(id_lote)
 );
 
-create table t_hardware (
-	idTipo int not null primary key auto_increment,
-    tipo enum ('CPU','RAM','DISCO')
+CREATE TABLE t_hardware (
+	idTipo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tipo ENUM ('CPU','RAM','DISCO'),
+    unidadeMedida VARCHAR(10)
 );
 
-create table hardware (
-	idHardware int not null primary key,
-    fkTipo int not null,
-    fkVeiculo int not null,
-    parametro decimal(5,2) not null,
-    constraint fk_tipo foreign key (fkTipo) references t_hardware(idTipo),
-    constraint fk_veiculo foreign key (fkVeiculo) references veiculo(id_veiculo)
+CREATE TABLE hardware (
+	idHardware INT NOT NULL PRIMARY KEY,
+    fkTipo INT NOT NULL,
+    fkVeiculo INT NOT NULL,
+    parametro INT NOT NULL,
+    CONSTRAINT fk_tipo FOREIGN KEY (fkTipo) REFERENCES t_hardware(idTipo),
+    CONSTRAINT fk_veiculo FOREIGN KEY (fkVeiculo) REFERENCES veiculo(id_veiculo)
 );
 
-
-create table alerta (
-	id_alerta INT NOT NULL auto_increment,
-    descricao text,
-    nivel enum('baixo', 'medio', 'alto') NOT NULL,
-    data_alerta DATETIME NOT NULL default CURRENT_TIMESTAMP,
-    status enum('pendente', 'em análise', 'resolvido') NOT NULL, 
-    prioridade int not null,
+CREATE TABLE alerta (
+	id_alerta INT NOT NULL AUTO_INCREMENT,
+    descricao TEXT,
+    nivel ENUM('baixo', 'medio', 'alto') NOT NULL,
+    data_alerta DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('pendente', 'em análise', 'resolvido') NOT NULL, 
+    prioridade INT NOT NULL,
     PRIMARY KEY (id_alerta),
     CONSTRAINT chk_prioridade CHECK (prioridade IN (1, 2, 3, 4, 5))
 );
 
-create table hard_alerta (
-	idHAlerta int not null auto_increment,
-	fkAlerta int not null,
-    fkHardware int not null,
-    quantidade int,
-    primary key(idHAlerta, fkAlerta, fkHardware),
-    constraint fk_Alerta foreign key (fkAlerta) references alerta(id_Alerta),
-    constraint fk__Hardware foreign key (fkHardware) references hardware(idHardware)
+CREATE TABLE hard_alerta (
+	idHAlerta INT NOT NULL AUTO_INCREMENT,
+	fkAlerta INT NOT NULL,
+    fkHardware INT NOT NULL,
+    quantidade INT,
+    PRIMARY KEY(idHAlerta, fkAlerta, fkHardware),
+    CONSTRAINT fk_Alerta FOREIGN KEY (fkAlerta) REFERENCES alerta(id_Alerta),
+    CONSTRAINT fk__Hardware FOREIGN KEY (fkHardware) REFERENCES hardware(idHardware)
+);DROP DATABASE IF EXISTS navix;
+CREATE DATABASE navix;
+USE navix;
+
+CREATE TABLE endereco (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    rua VARCHAR(300) NOT NULL,
+    numero INT NOT NULL,
+    cep CHAR(8) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(200) NOT NULL,
+    estado VARCHAR(100) NOT NULL,
+    pais VARCHAR(200) NOT NULL
 );
 
+CREATE TABLE empresa (
+	id_empresa INT NOT NULL AUTO_INCREMENT,
+    razaoSocial VARCHAR(50) NOT NULL,
+    cnpj VARCHAR(14) NOT NULL,
+    fkEndereco INT NOT NULL,
+    PRIMARY KEY (id_empresa),
+    CONSTRAINT fkendereco FOREIGN KEY (fkEndereco) REFERENCES endereco(id)
+);
+
+CREATE TABLE funcionario (
+	id_funcionario INT NOT NULL AUTO_INCREMENT,
+    fkEmpresa INT,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(50) NOT NULL,
+    telefone VARCHAR(11),
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(250) NOT NULL,
+    cargo BOOLEAN NOT NULL,
+	caminhoImagem VARCHAR(500) NOT NULL DEFAULT '../assets/img/foto-usuario.png',
+    CONSTRAINT chk_telefone CHECK (CHAR_LENGTH(telefone) IN (10, 11)),
+    PRIMARY KEY (id_funcionario),
+    UNIQUE (email), 
+    CONSTRAINT fk_empresa_funcionario FOREIGN KEY (fkEmpresa) REFERENCES empresa(id_empresa)
+);
+
+CREATE TABLE lote (
+	id_lote INT NOT NULL AUTO_INCREMENT,
+    codigo_lote VARCHAR(50) NOT NULL, 
+    data_fabricacao DATE,
+    PRIMARY KEY(id_lote),
+    UNIQUE(codigo_lote)
+);
+
+CREATE TABLE veiculo (
+	id_veiculo INT NOT NULL AUTO_INCREMENT,
+    fkempresa INT NOT NULL,
+    fklote INT NOT NULL,
+    modelo VARCHAR(50),
+    pilotoAuto_versao VARCHAR(20),
+    data_ativacao DATE,
+    status ENUM('ativo', 'manutenção', 'inativo'),
+    PRIMARY KEY (id_veiculo),
+    CONSTRAINT fk_veiculo_empresa FOREIGN KEY (fkempresa) REFERENCES empresa(id_empresa),
+    CONSTRAINT fk_veiculo_lote FOREIGN KEY (fklote) REFERENCES lote(id_lote)
+);
+
+CREATE TABLE t_hardware (
+	idTipo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tipo ENUM ('CPU','RAM','DISCO'),
+    unidadeMedida VARCHAR(10)
+);
+
+CREATE TABLE hardware (
+	idHardware INT NOT NULL PRIMARY KEY,
+    fkTipo INT NOT NULL,
+    fkVeiculo INT NOT NULL,
+    parametro INT NOT NULL,
+    CONSTRAINT fk_tipo FOREIGN KEY (fkTipo) REFERENCES t_hardware(idTipo),
+    CONSTRAINT fk_veiculo FOREIGN KEY (fkVeiculo) REFERENCES veiculo(id_veiculo)
+);
+
+CREATE TABLE alerta (
+	id_alerta INT NOT NULL AUTO_INCREMENT,
+    descricao TEXT,
+    nivel ENUM('baixo', 'medio', 'alto') NOT NULL,
+    data_alerta DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('pendente', 'em análise', 'resolvido') NOT NULL, 
+    prioridade INT NOT NULL,
+    PRIMARY KEY (id_alerta),
+    CONSTRAINT chk_prioridade CHECK (prioridade IN (1, 2, 3, 4, 5))
+);
+
+CREATE TABLE hard_alerta (
+	idHAlerta INT NOT NULL AUTO_INCREMENT,
+	fkAlerta INT NOT NULL,
+    fkHardware INT NOT NULL,
+    quantidade INT,
+    PRIMARY KEY(idHAlerta, fkAlerta, fkHardware),
+    CONSTRAINT fk_Alerta FOREIGN KEY (fkAlerta) REFERENCES alerta(id_Alerta),
+    CONSTRAINT fk__Hardware FOREIGN KEY (fkHardware) REFERENCES hardware(idHardware)
+);
