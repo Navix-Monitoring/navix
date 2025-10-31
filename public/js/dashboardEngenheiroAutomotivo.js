b66c33a736182d19c258d85b5aa328e6013303e4
CelinaBenedito <celina.benedito@sptech.school>
2025-10-30 23:08:51 -0300

    Fix: dashboard do engenheiro automotivo

--- public/js/dashboardEngenheiroAutomotivo.js
+++ public/js/dashboardEngenheiroAutomotivo.js
@@ -1,35 +1,84 @@
-function listarLotes() {
-    console.log("Estou na funcão listar lotes...")
+let listaModelos;
 
-    fetch(`/dashboard/listar/${sessionStorage.id_empresa_ss}`, {
+function listarModelos(){
+     modelos.innerHTML = `
+     <div class="cabecalho">
+     <p>Modelo</p>
+     <p>Status</p>
+     </p>Falhas detectadas</p>
+     <p style="text-align: center;">Componentes <br>monitorados</p>
+     <p>Taxa de falha</p>
+     </div>
+     `
+
+     fetch(`/dashboard/listarModelos`,{
         method: "GET"
-    }).then(res => {
-        res.json().then(json => {
-            console.log("Entrei na resposta do fetch do listar lotes")
-            const listaLotes = json;
-            for (let c = 0; json.length > c; c++) {
-                console.log("Entrei no listar lotes")
-                if (c % 2 == 0) {
-                    lotes.innerHTML +=
-                        `
-                        <a href="lote.html" onclick="localStorage.setItem('lote', ${json[c].id})">
-                        <div class="bg-black p-4 rounded-lg text-white cards-lotes"><p>Lote: ${listaLotes[c].codigo_lote}</p><p> Status: ${listaLotes[c].status}</p> <button onclick="buscarLote(${c + 1})" class="cursor-pointer w-150px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Editar</button> <button class="cursor-pointer w-30px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Remover</button></div>
-                       <a>
-                        `
-                }
-                else {
-                    lotes.innerHTML +=
-                        `
-                        <a href="lote.html" onclick="localStorage.setItem('lote', ${json[c].id})">
+    }).then(res=>{
+        res.json().then(json =>{
+            console.log("Entrei na resposta do fetch do listar modelos")
 
-                            <div class="bg-blue-900 p-4 rounded-lg text-white cards-lotes"><p>Lote: ${listaLotes[c].codigo_lote}</p><p> Status: ${listaLotes[c].status}</p> <button onclick="buscarLote(${c + 1})" class="cursor-pointer w-150px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Editar</button> <button class="cursor-pointer w-30px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Remover</button></div>
-                        </a>
-            `
-                }
-
+            listaModelos = json;
+            for(let c = 0; c<listaModelos.length;c++){
+                modelos.innerHTML += 
+                `
+                            <div class="bg-blue-900 p-4 rounded-lg text-white cards-modelos">
+                                <p>${listaModelos[c].nome}</p>
+                                <p> ${listaModelos[c].status}</p>
+                                <p>223</p>
+                                <p>3</p>
+                                <p>2% </p> 
+                                 <button onclick="buscarLote(${c + 1})" class="cursor-pointer w-150px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Editar</button> 
+                                <button class="cursor-pointer w-30px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Remover</button>
+                            </div>
+                `
             }
-
         })
     })
+}
 
+function filtrar(){
+    modelos.innerHTML = "";
+    let busca = select_Status.value;
+    if(busca != "#"){
+        if(busca=="ati"){
+            for(let c = 0; c<listaModelos.length;c++){
+                if(listaModelos[c].status == "Ativo"){
+                     modelos.innerHTML += 
+                `
+                            <div class="bg-blue-900 p-4 rounded-lg text-white cards-modelos">
+                                <p>Modelo: ${listaModelos[c].nome}</p>
+                                <p> Status: ${listaModelos[c].status}</p>
+                                <p>Falhas detectadas:</p>
+                                <p>Componentes Monitorados:</p>
+                                <p>Taxa de Falha: </p> 
+                                <button onclick="buscarLote(${c + 1})" class="cursor-pointer w-150px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Editar</button> 
+                                <button class="cursor-pointer w-30px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Remover</button>
+                            </div>
+                `
+                }
+               
+            }
+        }
+        else if(busca=="des"){
+            for(let c = 0; c<listaModelos.length;c++){
+                if(listaModelos[c].status == "Descontinuado"){
+                     modelos.innerHTML += 
+                `
+                            <div class="bg-blue-900 p-4 rounded-lg text-white cards-modelos">
+                                <p>Modelo: ${listaModelos[c].nome}</p>
+                                <p> Status: ${listaModelos[c].status}</p>
+                                <p>Falhas detectadas:</p>
+                                <p>Componentes Monitorados:</p>
+                                <p>Taxa de Falha: </p> 
+                                <button onclick="buscarLote(${c + 1})" class="cursor-pointer w-150px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Editar</button> 
+                                <button class="cursor-pointer w-30px bg-white border-2 border-blue-900 text-black rounded-md text-center py-2 hover:text-blue-900 hover:font-medium">Remover</button>
+                            </div>
+                `
+                }
+               
+            }
+        }
+    }else{
+        listarModelos();
+    }
 }
